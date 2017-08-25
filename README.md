cfa8552585abbc9c306574a8d56de280aa65aea7
rachaelhuber <rachaelsmith.email@gmail.com>
2017-08-26 00:16:25 +0200

    Improved Quicksort

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # go-go
-Data structures and Algorithms
+Data Structures and Algorithms
 
 Implemented:
 
 Sorting:
 * BubbleSort - O(n2) Stable 
 * SelectionSort - O(n2) Not Stable
+* QuickSort - O(nlogn) but could be O(n2)
+
+Searching:
--- src/data-structures/Sorting/QuickSorter.cs
+++ src/data-structures/Sorting/QuickSorter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace data_structures.Sorting
 {
@@ -7,22 +8,21 @@ namespace data_structures.Sorting
 		// Quick sort
 		// Use a pivot to move all the smaller items to the left and others greater to the right
 		// Swap as neccessary
-		// Then do the same on subarray
+		// Then do the same on each subarray
 		// Good case O(nlogn)
 		// Bad case O(nlogn)
 		public int[] Sort(int[] array)
 		{
-			if(array.Length < 2)
+			if (array.Length < 2)
 			{
 				return array;
 			}
 
-			// ok good luck
 			var pivot = array[array.Length - 1];
 
 			var wall = 0;
 
-			for(int i = 0; i < array.Length - 1 ; i++)
+			for (int i = 0; i < array.Length - 1; i++)
 			{
 				if (array[i] < pivot)
 				{
@@ -38,38 +38,10 @@ namespace data_structures.Sorting
 			array[wall] = array[array.Length - 1];
 			array[array.Length - 1] = rightOfWall2;
 
-			var arrayLessThanPivot = CopyArray(array, 0, wall - 1);
-			var arrayMoreThanPivot = CopyArray(array, wall, array.Length - 1);
-			array = AddArrays(Sort(arrayLessThanPivot) , Sort(arrayMoreThanPivot));
-			return array;
-		}
+			var arrayLessThanPivot = array.Take(wall).ToArray();
+			var arrayMoreThanPivot = array.Skip(wall).ToArray();
 
-		private int[] AddArrays(int[] a1, int[] a2)
-		{
-			var array = new int[a1.Length + a2.Length];
-		    for (int i = 0; i < a1.Length; i++)
-			{
-				array[i] = a1[i];
-			}
-
-			for (int i = 0; i < a2.Length; i++)
-			{
-				array[a1.Length + i] = a2[i];
-			}
-
-			return array;
-		}
-
-		private int[] CopyArray(int[] array, int start, int end)
-		{
-			var newArray = new int[end-start + 1];
-
-			for (int i = 0; i<end-start + 1; i++ )
-			{
-				newArray[i] = array[start + i];
-			}
-
-			return newArray;
+			return Sort(arrayLessThanPivot).Concat(Sort(arrayMoreThanPivot)).ToArray();
 		}
 	}
 }
